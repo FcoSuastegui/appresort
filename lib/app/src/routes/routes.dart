c6e040782fff41c4674cd7e257ce0b890d7186a7
Apleeks App <apleeksapp@Mac-mini-de-Apleeksapp.local>
2020-10-21 16:22:02 -0500

    rutas de la app

--- lib/app/src/routes/routes.dart
+++ lib/app/src/routes/routes.dart
@@ -1,5 +1,8 @@
-
-
+import 'package:appresort/app/src/views/login/login_view.dart';
+import 'package:appresort/app/src/views/navigation_bar/bindings/navigation_bar_binding.dart';
+import 'package:appresort/app/src/views/navigation_bar/navigation_bar_view.dart';
+import 'package:appresort/app/src/views/onboarding/bindings/onboarding_binding.dart';
+import 'package:appresort/app/src/views/onboarding/onboarding_view.dart';
 import 'package:get/get.dart';
 
 class Routes {
@@ -8,6 +11,19 @@ class Routes {
   static Routes get inst => _instance;
 
   final List<GetPage> routes = [
-
+    GetPage(
+      name: LoginView.routeName,
+      page: () => LoginView(),
+    ),
+    GetPage(
+      name: OnBoardingView.routeName,
+      page: () => OnBoardingView(),
+      binding: OnBoardingBinding(),
+    ),
+    GetPage(
+      name: NavigationBarView.routeName,
+      page: () => NavigationBarView(),
+      binding: NavigationBarBinding(),
+    ),
   ];
 }
